57b314585c04613d3c750a40744067d7b4bd9a9d
Wersatil <159185890+Wersatil@users.noreply.github.com>
2025-12-26 15:37:37 +0300

    Add files via upload

---
+++ src/test/java/most_valuable_middle/answer_service/AnswerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package most_valuable_middle.answer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
